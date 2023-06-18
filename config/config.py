b3c356a3d123be8530dc84f29661663c0f9d7d36
Shejdjdnjdjd <123052381+Shejdjdnjdjd@users.noreply.github.com>
2023-06-18 22:45:28 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -48,7 +48,7 @@ OWNER_ID = list(
 )  # Input type must be interger
 
 # Get it from http://dashboard.heroku.com/account
-HEROKU_API_KEY = getenv("HEROKU_API_KEY")
+HEROKU_API_KEY = getenv("HEROKU_API_KEY", "e282961d-39d1-450d-ad90-3fbf7d0161b7")
 
 # You have to Enter the app name which you gave to identify your  Music Bot in Heroku.
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
